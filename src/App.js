1936cda07f0f122e18db56f9da9128c53d132493
Ifycode <marychord@gmail.com>
2022-02-03 01:16:59 +0100

    Recap: Outputing lists

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import { useState } from 'react';
 function App() {
 
   const [name, setName] = useState('Maria');
+  const [events, setEvents] = useState([
+    {title: 'Title one', id: 1},
+    {title: 'Title two', id: 2},
+    {title: 'Title three', id: 3},
+  ]);
 
   const handleClick = () => {
     setName('Ifeoma');
@@ -13,6 +18,11 @@ function App() {
     <div className="App">
       <h1>My name is {name}</h1>
       <button onClick={handleClick}>Change name</button>
+      {events.map((event) => (
+        <div key={event.id}>
+          <h2>{event.title}</h2>
+        </div>
+      ))}
     </div>
   );
 }
