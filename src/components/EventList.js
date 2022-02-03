3d39bdbc3ff0c9ae56d8dec7c44dcc4148a4b6b7
Ifycode <marychord@gmail.com>
2022-02-04 00:14:03 +0100

    Learning: CSS modules

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,13 +1,16 @@
 import React from "react";
+import styles from "./EventList.module.css";
 
 export default function EventList({ events, handleEventDeleteClick }) {
   return (
     <>
         {events.map((event) => (
-            <React.Fragment key={event.id}>
+            //<React.Fragment key={event.id}>
+            <div className={styles.card} key={event.id}>
                 <h2>{event.title}</h2>
                 <button onClick={() => {handleEventDeleteClick(event.id)}}>Delete event</button>
-            </React.Fragment>
+            </div>
+            //</React.Fragment>
         ))}
     </>
   );
