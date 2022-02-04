3c544e945326a8bcd7cab9febe17c06d2a049a45
Ifycode <marychord@gmail.com>
2022-02-04 19:33:40 +0100

    Recap: Using the useRef hook

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -1,21 +1,21 @@
-import { useState } from 'react';
+import { useRef } from 'react';
 import './NewEventForm.css';
 
 export default function NewEventForm({ addEvent }) {
-    const [title, setTitle] = useState('');
-    const [date, setDate] = useState('');
+    const title = useRef();
+    const date = useRef();
 
     const resetForm = () => {
-        setTitle('');
-        setDate('');
+       title.current.value = '';
+       date.current.value = '';
     }
 
     const handleSubmit = (e) => {
         e.preventDefault();
 
         const event = {
-            title: title,
-            date: date,
+            title: title.current.value,
+            date: date.current.value,
             id: Math.floor(Math.random() * 10000)
         }
 
@@ -30,19 +30,17 @@ export default function NewEventForm({ addEvent }) {
     return (
         <form className="new-event-form" onSubmit={handleSubmit}>
             <label>
-                <span>Event Title: {title} </span>
+                <span>Event Title: </span>
                 <input 
                     type="text" 
-                    onChange={(e) => setTitle(e.target.value)}
-                    value={title}
+                    ref={title}
                 />
             </label>
             <label>
-                <span>Event Date: {date} </span>
+                <span>Event Date: </span>
                 <input 
                     type="date" 
-                    onChange={(e) => setDate(e.target.value)}
-                    value={date}
+                    ref={date}
                 />
             </label>
             <button>Submit</button>
@@ -50,3 +48,58 @@ export default function NewEventForm({ addEvent }) {
         </form>
     );
 }
+
+
+//Using use state:
+// import { useState } from 'react';
+// import './NewEventForm.css';
+
+// export default function NewEventForm({ addEvent }) {
+//     const [title, setTitle] = useState('');
+//     const [date, setDate] = useState('');
+
+//     const resetForm = () => {
+//         setTitle('');
+//         setDate('');
+//     }
+
+//     const handleSubmit = (e) => {
+//         e.preventDefault();
+
+//         const event = {
+//             title: title,
+//             date: date,
+//             id: Math.floor(Math.random() * 10000)
+//         }
+
+//         addEvent(event);
+//         resetForm();
+//     }
+
+//     // const handleChange = (e) => {
+//     //     setTitle(e.target.value);
+//     // }
+
+//     return (
+//         <form className="new-event-form" onSubmit={handleSubmit}>
+//             <label>
+//                 <span>Event Title: {title} </span>
+//                 <input 
+//                     type="text" 
+//                     onChange={(e) => setTitle(e.target.value)}
+//                     value={title}
+//                 />
+//             </label>
+//             <label>
+//                 <span>Event Date: {date} </span>
+//                 <input 
+//                     type="date" 
+//                     onChange={(e) => setDate(e.target.value)}
+//                     value={date}
+//                 />
+//             </label>
+//             <button>Submit</button>
+//             <p onClick={resetForm}>Reset form</p>
+//         </form>
+//     );
+// }
