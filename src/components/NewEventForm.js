bf48a6a47be7de70221bec067a3504506f1c10b8
Ifycode <marychord@gmail.com>
2022-02-04 16:02:03 +0100

    Learning: controlled input

--- src/components/NewEventForm.js
+++ src/components/NewEventForm.js
@@ -5,6 +5,11 @@ export default function NewEventForm() {
     const [title, setTitle] = useState('');
     const [date, setDate] = useState('');
 
+    const resetForm = () => {
+        setTitle('');
+        setDate('');
+    }
+
     // const handleChange = (e) => {
     //     setTitle(e.target.value);
     // }
@@ -13,13 +18,22 @@ export default function NewEventForm() {
         <form className="new-event-form">
             <label>
                 <span>Event Title: {title} </span>
-                <input type="text" onChange={(e) => setTitle(e.target.value)} />
+                <input 
+                    type="text" 
+                    onChange={(e) => setTitle(e.target.value)}
+                    value={title}
+                />
             </label>
             <label>
                 <span>Event Date: {date} </span>
-                <input type="date" onChange={(e) => setDate(e.target.value)}/>
+                <input 
+                    type="date" 
+                    onChange={(e) => setDate(e.target.value)}
+                    value={date}
+                />
             </label>
             <button>Submit</button>
+            <p onClick={resetForm}>Reset form</p>
         </form>
     );
 }
